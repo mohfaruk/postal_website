71222b62d9e1424d00becb845cd18d7eaeb13f51
Mohamad Faruk <mohamadfaruk.uk~gmail.com>
2021-12-04 14:36:04 +0000

    Version 2 - pre deployment

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-// const navbar = document.getElementById("navbar");
-// let scrolled = false;
+// Navbar Hidden Effect
+const navbar = document.querySelector(".navbar");
+let scrolled = false;
 
-// window.onscroll = function () {
-//   //When scrolled above 100 pixels it removes the 'top' class
-//   if (window.pageYOffset > 100) {
-//     navbar.classList.remove("top");
-//     //When scrolled the navbar rises by 70 px
-//     if (!scrolled) {
-//       navbar.style.transform = "translateY(-70px)";
-//     }
-//     //Function removes it after 1 second
-//     setTimeout(function () {
-//       navbar.style.transform = "translateY(0)";
-//     }, 1000);
-//   } else {
-//     //Resets back to normal
-//     navbar.classList.add("top");
-//     scrolled = false;
-//   }
-// };
+window.onscroll = function () {
+  //When scrolled above 100 pixels it removes the 'top' class
+  if (window.pageYOffset > 100) {
+    //When scrolled the navbar rises by 70 px
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    //Function removes it after 1 second
+    setTimeout(function () {
+      navbar.style.transform = "translateY(0)";
+    }, 1000);
+  } else {
+    //Resets back to normal
+    navbar.classList.add("top");
+    scrolled = false;
+  }
+};
 
-function togglePopup() {
-  document.getElementById("popup-1").classList.toggle("active");
-}
+// Smooth Scrolling
+$(".navbar a").on("click", function (event) {
+  //When navbar link is clicked
+  if (this.hash !== "") {
+    //When it detects section with initial #
+    const hash = this.hash;
+
+    //Executes smooth scroll
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top, //Returns certical scrollbar position
+      },
+      800
+    );
+  }
+});
